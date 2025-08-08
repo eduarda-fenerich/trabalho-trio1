a07137c04148a7071db1beea7438c1c90e5954fc
eduarda-fenerich <fenericheduarda13@gmail.com>
2025-08-08 12:22:02 -0300

    Add files via upload

--- Loja/script.js
+++ Loja/script.js
@@ -23,6 +23,47 @@ document.addEventListener("DOMContentLoaded", function(){
   fetch('../Dados/mock.json').then((response) => response.json())
                              .then((data) => {
                               produtos = data
-                              console.log(data)
+                              //console.log(data)
+
+                              const produtosContainer = document.getElementById('produtos-container')
+                              produtos.forEach((produto, index) => {
+
+                                const card = document.createElement('div')
+                                card.className = 'card'
+                                card.style.width = '18rem'
+                                card.style.marginRight = '10px'
+
+                                const imagem = document.createElement('img')
+                                imagem.src = produto.imagem
+                                imagem.className = 'card-img-top'
+
+                                const cardBody = document.createElement('div')
+                                cardBody.className = 'card-body'
+
+                                const cardTitle = document.createElement('h5')
+                                cardTitle.className = 'card-text'
+                                cardTitle.textContent = produto.descricao
+
+                                const cardText = document.createElement('p')
+                                cardText.className = 'card-text'
+                                cardText.textContent = 'Preço: $' + produto.preco.toFixed(2)
+
+                                const btnAdicionarAoCarrinho = document.createElement('a')
+                                btnAdicionarAoCarrinho.href = '#'
+                                btnAdicionarAoCarrinho.className = 'btn btn-primary btn-adicionar-ao-carrinho'
+                                btnAdicionarAoCarrinho.textContent = 'Adicionar ao carrinho'
+                                btnAdicionarAoCarrinho.setAttribute('data-indice', index)
+
+                                //criando os pais e filhos segundo o bootstrap
+                                cardBody.appendChild(cardTitle)
+                                cardBody.appendChild(cardText)
+                                cardBody.appendChild(btnAdicionarAoCarrinho)
+
+                                card.appendChild(imagem)
+                                card.appendChild(cardBody)
+
+                                produtosContainer.appendChild(card)
+                              });
+                                
                              }).catch((error) => console.error('Erro ao carregar os dados', error))
 });
